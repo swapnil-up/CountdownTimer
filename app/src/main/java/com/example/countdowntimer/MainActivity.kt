0e59dbb10f6627a53b0ed539f31bddbbb3f462c7
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-10-08 12:08:15 +0545

    Made the buttons work. Learnt how to work with CountDownTimer object which was cool. Could add more stuff later. Three number pickers to represent hours, minutes, seconds. A blinking textview for finished. And making the numberpicker show 01,02, 03.. instead of 1,2,3.

--- app/src/main/java/com/example/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.countdowntimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var resetButton: Button
     private lateinit var startButton: Button
     private lateinit var pauseButton: Button
+    private var timer:CountDownTimer?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,17 +33,31 @@ class MainActivity : AppCompatActivity() {
         numberPicker.maxValue=600
 
         startButton.setOnClickListener{
-            val time=numberPicker.value.toLong()*1000
+            val time=(numberPicker.value+1).toLong()*1000
             startTimer(time)
+        }
+        resetButton.setOnClickListener{
+            countdownText.text="00:00"
+            timer?.cancel()
+        }
+        pauseButton.setOnClickListener{
+            timer?.cancel()
         }
     }
     private fun startTimer(duration:Long){
-        var minute= duration/60
-        var second=duration%60
-        countdownText.text = minute.toString()+second.toString()
-        if (duration.toInt()==0) {
-            Toast.makeText(this, "Timer Finished", Toast.LENGTH_SHORT).show()
-            countdownText.text = "Finished"
+        timer?.cancel()
+        timer=object : CountDownTimer(duration,1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val minutes= (millisUntilFinished/1000)/60
+                val seconds=(millisUntilFinished/1000)%60
+                countdownText.text=String.format("%02d:%02d",minutes,seconds)
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@MainActivity, "Timer Finished", Toast.LENGTH_SHORT).show()
+                countdownText.text = "Finished"
+            }
         }
+        timer?.start()
     }
 }
